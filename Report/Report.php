d89e0ea3fa17fb20b4f7a340f2ceec17b7368762
ellery44 <addingtonwhiteem@beloit.edu>
2015-04-25 18:27:33 -0500

    Adds remaining TODO's from code review shows example of URL's not being dynamic. Kevin will fix this.

--- Report/Report.php
+++ Report/Report.php
@@ -65,6 +65,7 @@ class Report extends PluginBase
             $event = $this->event;
             $menu = $event->get('menu', array());
             $menu['items']['left'][] = array(
+                //TODO Make this URL Dynamic
                 'href' => "/index.php/plugins/direct?plugin=Report&function=managePrograms",
                 'alt' => gT('CA Report'),
                 'image' => 'chart_bar.png',
