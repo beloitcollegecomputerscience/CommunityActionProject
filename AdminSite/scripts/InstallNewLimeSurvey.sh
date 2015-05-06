7816d958f0ec5e507f7f2f82a1ee93f2b6320c74
ellery44 <addingtonwhiteem@beloit.edu>
2015-05-06 10:31:38 -0500

    Adds automatic creation of new db and db user for each individual install of lime survey to install script

--- AdminSite/scripts/InstallNewLimeSurvey.sh
+++ AdminSite/scripts/InstallNewLimeSurvey.sh
@@ -29,3 +29,17 @@ chown -R apache  ~/public_html/${GROUPNAME}/upload/;
 #Set Permisions for application/config
 chmod -R 755  ~/public_html/${GROUPNAME}application/config;
 chown -R apache  ~/public_html/${GROUPNAME}/application/config;
+
+
+### Create New DataBase and user ###
+
+#Generate Password
+#Note: Need to have makepasswd installed run 'apt-get install makepasswd'
+echo makepasswd --chars 16 | newPassword;
+
+#execute sql command
+#Note: must have your .my.conf set up properly
+mysql -h localhost -e "create database ${GROUPNAME}; GRANT ALL PRIVILEGES ON ${GROUPNAME}.* TO ${GROUPNAME}_user@localhost IDENTIFIED BY '${newPassword}'";
+
+#Echo out pass word for sql user for now We should probably catch this some where
+echo ${newPassword};
